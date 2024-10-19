1c451d5b7195f289301f6c76376a34e7098157ff
TaeHoon <dhkttms3@gmail.com>
2024-10-19 17:36:12 +0900

    fix next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,  // React Strict Mode 활성화
-  swcMinify: true,  // SWC 기반의 빠른 빌드 압축 활성화
-  basePath: '/contact',
-};
+const nextConfig = {};
 
 export default nextConfig;
-
