e80e2b3ecc9cd7eaf34f856e95029e9fe44ed68c
태훈 <113021892+hctaehoon@users.noreply.github.com>
2024-10-20 16:11:29 +0900

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,26 +1,24 @@
-// lib/auth.ts
 import jwt from 'jsonwebtoken';
 
-const SECRET_KEY = process.env.SECRET_KEY || 'default_secret_key';  // 환경 변수를 사용
+const SECRET_KEY = process.env.SECRET_KEY || 'default_secret_key';
+const REFRESH_SECRET_KEY = process.env.REFRESH_SECRET_KEY || 'default_refresh_secret_key';
 
-// JWT 토큰 발급
+// 액세스 토큰 발급
 export function signToken(payload: object) {
-  return jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' }); // 유효기간 1시간
+  return jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
 }
 
-// JWT 토큰 검증 및 에러 처리
+// 리프레시 토큰 발급
+export function signRefreshToken(payload: object) {
+  return jwt.sign(payload, REFRESH_SECRET_KEY, { expiresIn: '60d' });
+}
+
+// JWT 토큰 검증
 export function verifyToken(token: string) {
   try {
-    // 토큰이 유효한 경우 디코딩된 정보를 반환
     return jwt.verify(token, SECRET_KEY);
-  } catch (error: any) {
-    // 에러가 토큰 만료로 인한 것인지 확인
-    if (error.name === 'TokenExpiredError') {
-      console.error("Token has expired:", error);
-      return { error: 'TokenExpired', message: '토큰이 만료되었습니다.' };
-    }
-    // 기타 에러 (토큰이 유효하지 않음 등)
-    console.error("Invalid token:", error);
-    return { error: 'InvalidToken', message: '유효하지 않은 토큰입니다.' };
+  } catch (error) {
+    console.error("Error auth.ts", error);
+    return null;
   }
 }
