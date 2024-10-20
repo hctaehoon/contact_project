123cc5c2bb10906bffef2e9a37247ffab71dd186
태훈 <113021892+hctaehoon@users.noreply.github.com>
2024-10-20 16:31:26 +0900

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,9 @@
+// contact_project/lib/auth.ts
+
 import jwt from 'jsonwebtoken';
 
-const SECRET_KEY = process.env.SECRET_KEY || 'default_secret_key';
-const REFRESH_SECRET_KEY = process.env.REFRESH_SECRET_KEY || 'default_refresh_secret_key';
+const SECRET_KEY = process.env.SECRET_KEY || 'default_secret_key';  // 액세스 토큰 비밀 키
+const REFRESH_SECRET_KEY = process.env.REFRESH_SECRET_KEY || 'default_refresh_secret_key';  // 리프레시 토큰 비밀 키
 
 // 액세스 토큰 발급
 export function signToken(payload: object) {
@@ -13,12 +15,22 @@ export function signRefreshToken(payload: object) {
   return jwt.sign(payload, REFRESH_SECRET_KEY, { expiresIn: '60d' });
 }
 
-// JWT 토큰 검증
-export function verifyToken(token: string) {
+// 액세스 토큰 검증
+export function verifyAccessToken(token: string) {
   try {
     return jwt.verify(token, SECRET_KEY);
   } catch (error) {
-    console.error("Error auth.ts", error);
-    return null;
+    console.error("Error during access token verification:", error);
+    throw error;
+  }
+}
+
+// 리프레시 토큰 검증
+export function verifyRefreshToken(token: string) {
+  try {
+    return jwt.verify(token, REFRESH_SECRET_KEY);
+  } catch (error) {
+    console.error("Error during refresh token verification:", error);
+    throw error;
   }
 }
