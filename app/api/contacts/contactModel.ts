9cfbcd87885de406262b9d1ab7c00b39e826de67
태훈 <113021892+hctaehoon@users.noreply.github.com>
2026-03-12 14:16:49 +0900

    fix data
    검사기술팀/  설비기술팀 분리

--- app/api/contacts/contactModel.ts
+++ app/api/contacts/contactModel.ts
@@ -3,7 +3,7 @@ import { contacts } from '@/data/contacts';
 
 export async function getContactsByDepartment() {
   return contacts.sort((a, b) => {
-    const order = ['대표', '경영지원팀', '생산관리팀', '품질관리팀', '설비/기술팀', '조장', '공정', '통근버스', '기타'];
+    const order = ['대표', '경영지원팀', '생산관리팀', '품질관리팀', '검사기술팀', '설비기술팀', '조장', '공정', '통근버스', '기타'];
     return order.indexOf(a.department) - order.indexOf(b.department);
   });
 }
